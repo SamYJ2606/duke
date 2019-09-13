368d0e270dfa79e40162dee84a1b20b6af1f64c7
SamYJ2606 <e0002804@u.nus.edu>
2019-09-13 15:21:49 +0800

    A-Level10.level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,24 @@
-import MyTask.*;
+import MyTask.Deadline;
+import MyTask.Events;
+import MyTask.MyTask;
+import MyTask.ToDo;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Duke {
+public class Duke extends Application {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+
+    public Duke() {
+
+    }
 
     public Duke(String filePath){
         this.ui = new Ui();
@@ -126,5 +137,14 @@ public class Duke {
 
     public static void main(String[] args) throws IOException {
         new Duke("duke.txt").run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
